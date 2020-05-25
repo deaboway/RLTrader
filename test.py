137f6b44597a18605d7c37606c9156427b53f6bd
Gavin <deaboway@163.com>
2020-05-25 18:28:55 +0800

    测试本地TensorFlow GPU是否正常work

--- test.py
+++ test.py
@@ -1,4 +1,10 @@
 import tensorflow as tf
+print('GPU', tf.test.is_gpu_available())
+
+a = tf.constant(2.)
+b = tf.constant(4.)
+print(a * b)
+
 hello = tf.constant('hello,TensorFlow!')
 sess = tf.Session()
 print(sess.run(hello))
